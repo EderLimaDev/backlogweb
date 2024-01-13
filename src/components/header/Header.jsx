20f2bc9d6ddd0a2eb5680f11c1733fb015799b25
EderLimaDev <ederlimarh@gmail.com>
2024-01-12 23:36:00 -0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+export const Header = () => {
+  return (
+    <div>
+        <header>
+            <h1>My Backlog</h1> 
+            <div className="login">Login</div>
+        </header>
+    </div>
+  )
+}
